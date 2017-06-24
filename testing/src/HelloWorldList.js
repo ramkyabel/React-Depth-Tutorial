21788b30a750e725b0a3bbf10c063f66470d2178
ramkyabel <ramky.gracia@gmail.com>
2017-06-24 13:13:49 -0400

    Added Remove Button to HelloWorld Component

--- testing/src/HelloWorldList.js
+++ testing/src/HelloWorldList.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
+import AddGreeter from './AddGreeter';
 import HelloWorld from './HelloWorld';
 import './HelloWorldList.css';
 
 class HelloWorldList extends Component {
 	constructor (props) {
 		super (props);
-		this.state = { greetings: ['Jim', 'Sally']};
+		this.state = { greetings: ['Jim', 'Sally', 'Bender']};
+		this.addGreeting = this.addGreeting.bind (this);
+		this.removeGreeting = this.removeGreeting.bind (this);
+	}
+	addGreeting (newName) {
+		this.setState ( {greetings: [...this.state.greetings, newName]});
+	} 
+	removeGreeting (removeName) {
+		const filteredGreetings = this.state.greetings.filter ((name) => 
+			 removeName !== name);
+		this.setState ( {greetings: filteredGreetings});
 	}
 	renderGreetings () {
-		return this.state.greetings.map (name => (
-			<HelloWorld key={name} name={name} />
-		));
+		return this.state.greetings.map (name => 
+			<HelloWorld 
+				key={name}
+				name={name}
+				removeGreeting={this.removeGreeting} />
+		);
 	}
 	render () {
 		return (
 			<div className="HelloWorldList">
+				<AddGreeter addGreeting={this.addGreeting} 
+										removeGreeting={this.removeGreeting}
+				/>
 				{this.renderGreetings ()}
 			</div>
 		);	
