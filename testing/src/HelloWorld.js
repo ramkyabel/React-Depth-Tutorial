12c4f09fc909c957ae1eeccad7267339fee0a389
ramkyabel <ramky.gracia@gmail.com>
2017-06-24 01:23:34 -0400

    Added state to Component

--- testing/src/HelloWorld.js
+++ testing/src/HelloWorld.js
@@ -1,1 +1,1 @@
-import React, { Component } from 'react';import './HelloWorld.css';class HelloWorld extends Component {	render () {		return (<div className="HelloWorld">Hello, {this.props.name} </div>);	}}export default HelloWorld;
+import React, { Component } from 'react';import './HelloWorld.css';class HelloWorld extends Component {	constructor (props) {		super (props);		this.state = {greeting: 'Hello'};	}	render () {		return (<div className="HelloWorld">{this.state.greeting}, {this.props.name} </div>);	}}export default HelloWorld;
